d46e16d6b18a426c92807ac976721c28f552d719
bhavithabandre2005-byte <bhavithabandre2005@gmail.com>
2026-07-15 20:01:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function registerStudent() { 
+alert("Register button clicked!"); 
+}
+function registerStudent() { 
+// Read the value typed in the Name textbox 
+let studentName = document.getElementById("name").value; 
+let email = document.getElementById("email").value;
+let mobile = document.getElementById("mobile").value;
+let age = document.getElementById("age").value;
+alert(studentName); 
+alert(email); 
+alert(mobile); 
+alert(age); 
+} 
